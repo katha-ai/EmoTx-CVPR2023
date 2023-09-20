25802ac04b72e9072400fc653aaa15429097798d
dhruvhacks <dhruv.srivastava@research.iiit.ac.in>
2023-09-20 21:48:17 +0530

    Added ResNet152_imagenet and ResNet50_places365 feature extractors

--- models/pretrained_models.py
+++ models/pretrained_models.py
@@ -1,3 +1,5 @@
+from torchvision import models
+
 import torch
 import torch.nn as nn
 
@@ -26,4 +28,33 @@ class MViT_v1(nn.Module):
             out (Tensor): Tensor of shape (batch_size, 768)
         """
         out = self.model(frames)
+        return out
+
+
+class Places365_ResNet50(nn.Module):
+    def __init__(self, weights_path, device):
+        super(Places365_ResNet50, self).__init__()
+        self.model = models.__dict__["resnet50"](num_classes=365)
+        ckpt = torch.load(weights_path, map_location=device)
+        state_dict = {str.replace(k,'module.',''): v for k,v in ckpt['state_dict'].items()}
+        self.model.load_state_dict(state_dict)
+        self.model = nn.Sequential(*(list(self.model.children())[:-1]))
+
+    def forward(self, imgs):
+        batch_size = imgs.shape[0]
+        out = self.model(imgs)
+        out = out.reshape(batch_size, -1)
+        return out
+
+
+class ImageNet_ResNet152(nn.Module):
+    def __init__(self):
+        super(ImageNet_ResNet152, self).__init__()
+        self.model = models.resnet152(pretrained=True)
+        self.model = nn.Sequential(*(list(self.model.children())[:-1]))
+
+    def forward(self, frames):
+        batch_size = frames.shape[0]
+        out = self.model(frames)
+        out = out.reshape(batch_size, -1)
         return out
